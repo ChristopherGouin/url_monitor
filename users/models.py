821b8b2e6ba686d23fad3d78eae981f118b9707d
christopher <christo.tpe@gmail.com>
2021-02-01 11:47:14 +0100

    Users: creation du model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class BaseModel(models.Model):
+  created = models.DateTimeField(auto_now_add = True, verbose_name="Date de création")
+  modified = models.DateTimeField(auto_now = True, verbose_name="Date de modification")
+
+  class Meta:
+    abstract = True
+    ordering = ("-created", )
+
+class UserProfile(AbstractUser, BaseModel):
+
+  display_name = models.TextField(
+    verbose_name= "Nom d'affichage",
+    default=None,
+    blank=True,
+    null=True
+  )
+
+  def __str__(self):
+    if self.display_name :
+      return self.display_name
+    else:
+      return self.username
+
+  class Meta:
+    verbose_name = "Utilisateur"
+    verbose_name_plural = "Utilisateurs"
+    ordering= ("created", )
